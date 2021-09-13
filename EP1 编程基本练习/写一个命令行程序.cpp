e6935700bd887810d7087169ca866830b41887a1
绝伦N <56631419+404name@users.noreply.github.com>
2021-09-13 16:53:15 +0800

    额

--- EP1 编程基本练习/写一个命令行程序.cpp
+++ EP1 编程基本练习/写一个命令行程序.cpp
@@ -3,22 +3,45 @@
 using namespace std;
 
 class Solution{
+	
 	public:
-		int check(vector<int> &arr){
-			int sum = 0;
-			for(auto &num:arr){
-				sum += num;
-				if(sum < 0) sum = 0;
-			}
-			return sum;
+		vector<int> sieve(int n) {
+			vector<int> vis(n,0);
+			vector<int> prime{0};
+			int cnt = 0;
+		    for(int i=2;i<=n;i++) {
+		        if(!vis[i]){
+		        	++cnt;
+		        	prime.push_back(i);
+				}
+		        for(int j=1;j<=cnt && i*prime[j]<=n;j++) {
+		            vis[i*prime[j]]=1;
+		            if(!(i%prime[j]))
+		                break;
+		        }
+		    }
+		    return prime;
 		}
+	
 }; 
 
 int main()
 {
     Solution solution;
-    vector<int> vec{1, -2, 3,-2, 5, 1};
-    cout << solution.check(vec) << endl;
+    vector<int> res =  solution.sieve(20);
+    int cnt = -1;
+    for(auto& prime:res){
+    	if(cnt == -1){
+    		cnt = 0;
+    		continue;
+		}
+    	cout << prime << ' ';
+    	cnt++;
+    	if(cnt == 5){
+    		cnt = 0;
+    		cout << endl;
+		}
+	}
     // return 7
     return 0;
 }
